6b826b9c836a9993ead1e6d860192f2a9eee9164
waleed <waleedelsefy@gmail.com>
2024-01-27 08:47:32 +0200

    add gtag head

--- header.php
+++ header.php
@@ -19,7 +19,28 @@ if (is_array($theme_settings)) {
     error_log("newaqar_theme_settings is not an array");
 }
 ?>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BNFNB9W1S6"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-BNFNB9W1S6');
+</script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-M56RJK4H');</script>
+<!-- End Google Tag Manager -->
+
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M56RJK4H"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header id="header" class="header" aria-label="Site header">
         <a id="site-link" href="<?php echo home_url(); ?>">
             <div class="logo">
