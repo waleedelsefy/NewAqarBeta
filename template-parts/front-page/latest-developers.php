21f282373cacf8a7eba5e43d1f6fbc2f6d398f4c
waleed <waleedelsefy@gmail.com>
2024-02-10 22:59:46 +0200

    fix developer page style

--- template-parts/front-page/latest-developers.php
+++ template-parts/front-page/latest-developers.php
@@ -15,30 +15,48 @@
         <div class="row">
             <div class="container">
                 <div class="swiper-container">
-                    <div class="swiper-wrapper">
-                        <?php
-                        $taxonomy = 'developer';
-                        $terms = get_terms($taxonomy);
-                        foreach ($terms as $term) {
-                            $term_details = get_term_by('id', $term->term_id, $taxonomy);
-                            if ($term_details) {
-                                $term_name = esc_html($term_details->name);
-                                $term_image = get_term_meta($term_details->term_id, 'developer_image', true);
-                                $term_link = get_term_link($term_details);
-                                if ($term_image) {
-                                    ?>
-                                    <div class="swiper-slide col-md-2 col-sm-4">
-                                        <a class="slider-card" href="<?php echo esc_url($term_link); ?>" target="_self" rel="dofollow" >
-                                            <img width="221" height="221" src="<?php echo esc_url($term_image); ?>" >
-                                        </a>
-                                        <h3 class="gs_logo_title" style="font-size: 16px; font-weight: 400;"><?php echo esc_html($term_name); ?></h3>
-                                    </div>
-                                    <?php
+                    <div class="swiper-container">
+                        <div class="swiper-wrapper">
+                            <?php
+                            $taxonomy = 'developer';
+                            $terms = get_terms($taxonomy);
+                            $count = 0; // Counter to track the number of terms displayed
+                            foreach ($terms as $term) {
+                                $term_details = get_term_by('id', $term->term_id, $taxonomy);
+                                if ($term_details) {
+                                    $term_name = esc_html($term_details->name);
+                                    $term_image = get_term_meta($term_details->term_id, 'developer_image', true);
+                                    $term_link = get_term_link($term_details);
+                                    if ($term_image && $count < 8) { // Check if term has image and count is less than 8
+                                        ?>
+                                        <div class="swiper-slide col-md-2 col-sm-4">
+                                            <a class="slider-card" href="<?php echo esc_url($term_link); ?>" target="_self" rel="dofollow">
+                                                <img width="221" height="221" src="<?php echo esc_url($term_image); ?>">
+                                            </a>
+                                            <h3 class="gs_logo_title" style="font-size: 16px; font-weight: 400;"><?php echo esc_html($term_name); ?></h3>
+                                        </div>
+                                        <?php
+                                        $count++; // Increment the counter
+                                    }
                                 }
+                                if ($count >= 8) break; // Break the loop once 8 terms are displayed
                             }
-                        }
-                        ?>
+                            ?>
+                        </div>
+                        <!-- Add Pagination -->
+                        <div class="swiper-pagination"></div>
                     </div>
+
+                    <script>
+                        var swiper = new Swiper('.swiper-container', {
+                            slidesPerView: 'auto',
+                            spaceBetween: 15,
+                            pagination: {
+                                el: '.swiper-pagination',
+                                clickable: true,
+                            },
+                        });
+                    </script>
                 </div>
             </div>
         </div>
