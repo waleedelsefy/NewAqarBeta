0607b1369e4052b9fdbad512484f001634754b01
walee <waleedelsefy@gmail.com>
2024-03-16 13:41:31 +0200

    get_the_content

--- single-templates/single-projects.php
+++ single-templates/single-projects.php
@@ -374,9 +374,8 @@ if ($city_terms && !is_wp_error($city_terms)) {
                         <div class="table-content my-2 py-3 px-3 ">
                             <?php  echo do_shortcode('[social_share_box]');?>
                             <?php echo generate_table_of_contents(); ?>
+                            <?php the_content(); ?>
 
-
-                            <?php  echo  get_the_content(); ?>
                         </div>
                         <?php
                         $faqs = isset($project_details['faqs']) ? esc_attr($project_details['faqs']) : '';
